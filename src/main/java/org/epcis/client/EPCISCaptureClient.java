30d6d59ef47a8afeb45e029586568e7b2f1d3c74
BeADia <koc0819@gmail.com>
2021-10-14 19:01:08 +0900

    downgrade capture client but still have error

--- src/main/java/org/epcis/client/EPCISCaptureClient.java
+++ src/main/java/org/epcis/client/EPCISCaptureClient.java
@@ -1,136 +1,34 @@
 package org.epcis.client;
 
-import org.epcis.document.builder.validator;
-import org.epcis.model.*;
-import org.epcis.util.UnitConverter;
+import org.epcis.model.EPCISDocumentType;
 import org.epcis.writer.json.JSONDocumentWriter;
 import org.epcis.writer.xml.XMLDocumentWriter;
+import org.json.JSONObject;
 import org.w3c.dom.Document;
 
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
-import javax.xml.namespace.QName;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.*;
-import java.math.BigDecimal;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.*;
 
+
+// TODO: Make Sample
+// TODO: solve related problem <- downgrade
 public class EPCISCaptureClient {
-    // Step 1: just create EPCISCaptureClient with url and document and send
-    // Step 2: enable to add an individual event as well as vocabulary. At this
-    // moment, very detailed validation will be conducted by validater
-    // I put validateAggregationEvent. Other events and vocabulary are same
-    // Step 3: epcisDocument to XML string is straightforward
-    // to Json is difficult. Good Luck!
-    // Step 4: fluent style event as well as vocabulary builder
-    // please focus on convenient aspect and cover whole data building
-    // TODO : name space simplify --> Root to Leaf clearNamespace
-    // TODO : pojo to json
-    // TODO : sensorData -> SensorElementBuilder 만들기
-    public static UnitConverter unitConverter;
-    public static List<String> namespaceMap;
     public URL captureEndpoint;
-    public EPCISDocumentType epcisDoc;
-
-    // event
-    public EPCISBodyType epcisBody;
-    public EventListType eventList;
-    public List<Object> events;
-
-    // vocabulary
-    public EPCISHeaderType epcisHeader;
-    public EPCISHeaderExtensionType epcisHeaderExtension;
-    public EPCISMasterDataType masterData;
-    public VocabularyListType vocaList;
-    public List<VocabularyType> vocabularies;
 
     public EPCISCaptureClient(String URL) {
         try {
             captureEndpoint = new URL(URL);
-            init();
         } catch (MalformedURLException e) {
             System.out.println("Wrong URL");
             e.printStackTrace();
         }
-    }
-
-    private void init() {
-        epcisDoc = new EPCISDocumentType();
-
-        // event
-        epcisBody = new EPCISBodyType();
-        eventList = new EventListType();
-        events = new ArrayList<Object>();
-        eventList.setObjectEventOrAggregationEventOrTransformationEvent(events);
-        epcisBody.setEventList(eventList);
-        epcisDoc.setEPCISBody(epcisBody);
-        // vocabulary
-        epcisHeader = new EPCISHeaderType();
-        epcisHeaderExtension = new EPCISHeaderExtensionType();
-        masterData = new EPCISMasterDataType();
-        vocaList = new VocabularyListType();
-        vocabularies = new ArrayList<VocabularyType>();
-        vocaList.setVocabulary(vocabularies);
-        masterData.setVocabularyList(vocaList);
-        epcisHeaderExtension.setEPCISMasterData(masterData);
-        epcisHeader.setExtension(epcisHeaderExtension);
-        epcisDoc.setEPCISHeader(epcisHeader);
-        // update namespaceMap
-        Map<QName, String> docAttrMap = epcisDoc.getOtherAttributes();
-        System.out.println(docAttrMap.size());
-        docAttrMap.forEach((qName, uri) -> {
-            System.out.println(uri);
-        });
-        this.setEPCISDoc();
-    }
-
-    public void addEvent(Object event) {
-        boolean isValid = false;
-        if (event instanceof AggregationEventType) {
-            isValid = validator.validateAggregationEvent((AggregationEventType) event);
-        } else if (event instanceof ObjectEventType) {
-            isValid = validator.validateObjectEvent((ObjectEventType) event);
-        } else if (event instanceof TransactionEventType) {
-            isValid = validator.validateTransactionEvent((TransactionEventType) event);
-        } else if (event instanceof TransformationEventType) {
-            isValid = validator.validateTransformationEvent((TransformationEventType) event);
-        } else if (event instanceof AssociationEventType) {
-            isValid = validator.validateAssociationEvent((AssociationEventType) event);
-        }
-        if (isValid) {
-            events.add(event);
-        } else {
-            System.out.println("Event is not valid");
-        }
-    }
-
-    public void addVoca(VocabularyType voca) {
-        this.vocabularies.add(voca);
-    }
-
-    public void clearEvent() {
-        events = new ArrayList<Object>();
-    }
-
-    public void clearVoca() {
-        vocabularies = new ArrayList<VocabularyType>();
-    }
-
-    public void clearAll() {
-        init();
-    }
-
-    public Document getEPCISDocument() {
-        return this.toXML();
     }
 
     public Integer sendByDocument(Document document) {
@@ -145,6 +43,49 @@ public class EPCISCaptureClient {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public Integer sendByJson(JSONObject json) {
+        String body = json.toString();
+        this.sendByString(body, true);
+        return null;
+    }
+
+    public Integer sendByEPCISDocument(EPCISDocumentType epcisDoc, boolean isJson) {
+        try {
+            String body;
+            if (isJson) {
+                // using json
+                body = this.toJsonLDString(epcisDoc);
+            } else {
+                // using xml
+                body = this.toXMLString(epcisDoc);
+            }
+            return sendByString(body, isJson);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String toXMLString(EPCISDocumentType epcisDoc) {
+        try {
+            Transformer tf = TransformerFactory.newInstance().newTransformer();
+            StringWriter sw = new StringWriter();
+            tf.setOutputProperty(OutputKeys.INDENT, "yes");
+            Document xml = XMLDocumentWriter.write(epcisDoc);
+            tf.transform(new DOMSource(xml), new StreamResult(sw));
+            return sw.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String toJsonLDString(EPCISDocumentType epcisDoc) {
+        org.bson.Document json = JSONDocumentWriter.write(epcisDoc);
+        return json.toJson();
     }
 
     public Integer sendByString(String body, boolean isJson) {
@@ -187,108 +128,5 @@ public class EPCISCaptureClient {
             e.printStackTrace();
         }
         return null;
-    }
-
-    public Integer sendByEPCISDocument(boolean byJson) {
-        try {
-            HttpURLConnection connection = (HttpURLConnection) this.captureEndpoint
-                    .openConnection();
-            connection.setRequestMethod("POST");
-            String body;
-            if (byJson) {
-                // using json
-                connection.setRequestProperty("Content-Type", "application/json");
-                connection.setDoOutput(true);
-                body = this.toJsonLDString();
-            } else {
-                // using xml
-                connection.setRequestProperty("Content-Type", "application/xml");
-                connection.setDoOutput(true);
-                body = this.toXMLString();
-            }
-            BufferedWriter bw = new BufferedWriter(
-                    new OutputStreamWriter(connection.getOutputStream()));
-            bw.write(body);
-            bw.flush();
-            bw.close();
-
-            // response handling
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream()));
-
-            // response http code
-            int responseCode = connection.getResponseCode();
-            if (responseCode == 400) {
-                System.out.println("400 ERROR");
-            } else if (responseCode == 500) {
-                System.out.println("500 ERROR");
-            } else {
-                System.out.println("200 SUCCESS");
-            }
-            String returnMsg = in.readLine();
-            System.out.println("RESPONSE" + returnMsg);
-            return responseCode;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public void setEPCISDoc() {
-
-        // set CreationDate
-        DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-        String dateValue = format.format(new Date());
-        Date date = null;
-        try {
-            date = format.parse(dateValue);
-
-            GregorianCalendar cal = new GregorianCalendar();
-            cal.setTime(date);
-            XMLGregorianCalendar xmlGreCal = null;
-            xmlGreCal = DatatypeFactory.newInstance().newXMLGregorianCalendar(cal);
-            this.epcisDoc.setCreationDate(xmlGreCal);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // setSchemaVersion
-        this.epcisDoc.setSchemaVersion(new BigDecimal("2.0"));
-
-        // set
-        System.out.println(this.epcisDoc.getOtherAttributes().toString());
-    }
-
-    public String toXMLString() {
-        try {
-            Transformer tf = TransformerFactory.newInstance().newTransformer();
-            StringWriter sw = new StringWriter();
-            tf.setOutputProperty(OutputKeys.INDENT, "yes");
-            tf.transform(new DOMSource(toXML()), new StreamResult(sw));
-            return sw.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public String toJsonLDString() {
-        String result = toJsonLD().toJson();
-        System.out.println(result);
-        return result;
-    }
-
-    public Document toXML() {
-        try {
-            return XMLDocumentWriter.write(this.epcisDoc);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public org.bson.Document toJsonLD() {
-        return JSONDocumentWriter.write(this.epcisDoc, this.events, this.vocabularies);
     }
 }
--- src/main/java/org/epcis/writer/json/JSONDocumentWriter.java
+++ src/main/java/org/epcis/writer/json/JSONDocumentWriter.java
@@ -1,66 +1,61 @@
 package org.epcis.writer.json;
 
+import org.bson.Document;
+import org.epcis.model.*;
+import org.epcis.model.exception.ValidationException;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bson.Document;
-import org.epcis.model.AggregationEventType;
-import org.epcis.model.AssociationEventType;
-import org.epcis.model.EPCISDocumentType;
-import org.epcis.model.ObjectEventType;
-import org.epcis.model.TransactionEventType;
-import org.epcis.model.TransformationEventType;
-import org.epcis.model.VocabularyType;
-import org.epcis.model.exception.ValidationException;
-
 public class JSONDocumentWriter {
-	public static Document write(EPCISDocumentType epcisDoc, List<Object> events,
-			List<VocabularyType> vocabularies) {
-		Document json = new Document();
-		json.put("isA", "EPCISDocument");
-		json.put("schemaVersion", epcisDoc.getSchemaVersion().toString());
-		json.put("creationDate", epcisDoc.getCreationDate().toString());
-		Document context = new Document();
-		// add namespace
+    public static Document write(EPCISDocumentType epcisDoc) {
+        Document json = new Document();
+        json.put("isA", "EPCISDocument");
+        json.put("schemaVersion", epcisDoc.getSchemaVersion().toString());
+        json.put("creationDate", epcisDoc.getCreationDate().toString());
+        Document context = new Document();
+        // add namespace
 
-		json.put("@context", context);
-		// event
-		ArrayList<Document> eventList = new ArrayList<Document>();
-		events.forEach(event -> {
-			try {
-				Document eventJson = null;
-				if (event instanceof AggregationEventType) {
-					eventJson = AggregationEventWriter.write((AggregationEventType) event);
-				} else if (event instanceof ObjectEventType) {
-					eventJson = ObjectEventWriter.write((ObjectEventType) event);
-				} else if (event instanceof TransactionEventType) {
-					eventJson = TransactionEventWriter.write((TransactionEventType) event);
-				} else if (event instanceof TransformationEventType) {
-					eventJson = TransformationEventWriter.write((TransformationEventType) event);
-				} else if (event instanceof AssociationEventType) {
-					eventJson = AssociationEventWriter.write((AssociationEventType) event);
-				}
-				if (eventJson != null) {
-					eventList.add(eventJson);
-				}
-			} catch (ValidationException e) {
-				e.printStackTrace();
-			}
-		});
-		Document epcisBodyJson = new Document();
-		epcisBodyJson.put("eventList", eventList);
-		json.put("epcisBody", epcisBodyJson);
-		// voca
-		List<Document> vocaList = new ArrayList<>();
-		vocabularies.forEach(voca -> {
-			vocaList.add(MasterDataWriter.write(voca));
-		});
-		Document epcisHeaderJson = new Document();
-		Document epcisMasterData = new Document();
-		epcisMasterData.put("vocabularyList", vocaList);
-		epcisHeaderJson.put("epcisMasterData", epcisMasterData);
-		json.put("epcisHeader", epcisHeaderJson);
+        json.put("@context", context);
+        // event
+        List<Object> events = epcisDoc.getEPCISBody().getEventList().getObjectEventOrAggregationEventOrTransformationEvent();
+        ArrayList<Document> eventList = new ArrayList<Document>();
+        events.forEach(event -> {
+            try {
+                Document eventJson = null;
+                if (event instanceof AggregationEventType) {
+                    eventJson = AggregationEventWriter.write((AggregationEventType) event);
+                } else if (event instanceof ObjectEventType) {
+                    eventJson = ObjectEventWriter.write((ObjectEventType) event);
+                } else if (event instanceof TransactionEventType) {
+                    eventJson = TransactionEventWriter.write((TransactionEventType) event);
+                } else if (event instanceof TransformationEventType) {
+                    eventJson = TransformationEventWriter.write((TransformationEventType) event);
+                } else if (event instanceof AssociationEventType) {
+                    eventJson = AssociationEventWriter.write((AssociationEventType) event);
+                }
+                if (eventJson != null) {
+                    eventList.add(eventJson);
+                }
+            } catch (ValidationException e) {
+                e.printStackTrace();
+            }
+        });
+        Document epcisBodyJson = new Document();
+        epcisBodyJson.put("eventList", eventList);
+        json.put("epcisBody", epcisBodyJson);
+        // voca
+        List<VocabularyType> vocas = epcisDoc.getEPCISHeader().getExtension().getEPCISMasterData().getVocabularyList().getVocabulary();
+        List<Document> vocaList = new ArrayList<>();
+        vocas.forEach(voca -> {
+            vocaList.add(MasterDataWriter.write(voca));
+        });
+        Document epcisHeaderJson = new Document();
+        Document epcisMasterData = new Document();
+        epcisMasterData.put("vocabularyList", vocaList);
+        epcisHeaderJson.put("epcisMasterData", epcisMasterData);
+        json.put("epcisHeader", epcisHeaderJson);
 
-		return json;
-	}
+        return json;
+    }
 }
