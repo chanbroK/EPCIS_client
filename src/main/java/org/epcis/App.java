31bafc3cf3dcafc784197ef2d9583a81a7beeacf
BeADia <koc0819@gmail.com>
2021-10-14 15:44:35 +0900

    first setting project

---
+++ src/main/java/org/epcis/App.java
@@ -0,0 +1,13 @@
+package org.epcis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
